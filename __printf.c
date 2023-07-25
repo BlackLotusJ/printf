d8822bf940f701a26f1be3174bf312005d07e8c5
Luke Kapesa <lukekapesa@gmail.com>
2023-07-25 16:25:33 -0700

    unsigned

--- __printf.c
+++ __printf.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main1.h"
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main1.h"
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
