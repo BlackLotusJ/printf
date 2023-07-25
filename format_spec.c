1078a3e49a19f498a8c786fa4104cd99e6a72c88
BlackLotusJ <jerrykiche61@gmail.com>
2023-07-25 16:59:24 +0300

    Function to print unssigned integers

--- format_spec.c
+++ format_spec.c
@@ -27,3 +27,29 @@ int print_char(va_list list)
 	_write(va_arg(list, int));
 	return (1);
 }
+/**
+ * print_int - prints an integer
+ * @list: list of args
+ * Return: character printed
+ */
+int print_int(va_list list)
+{
+	int num_length = print_num(list);
+	return (num_length);
+}
+/**
+ * unsigned_int - outputs unsigned integer
+ * @list: List of arguments
+ * Return: count of numbers
+ */
+int unsigned_int(va_list list)
+{
+	unsigned int num;
+
+	num = va_arg(list, unsigned int);
+	if (num == 0)
+		return (print_unsigned_num(num));
+	if (num < 1)
+		return (-1);
+	return (print_unsigned_number(num));
+}
