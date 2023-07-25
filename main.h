43f1cdc5c012cc2a9dc5881c3212c0a0e1009287
BlackLotusJ <jerrykiche61@gmail.com>
2023-07-25 20:38:29 +0300

    Added prototype to main.h

--- main.h
+++ main.h
@@ -11,6 +11,7 @@ int print_string(va_list list);
 int print_char(va_list list);
 int unsigned_int(va_list list);
 int print_int(va_list list);
+int parse(const char *form, conver_t f_list[], va_list arg_list);
 
 /*Helper Functions*/
 
