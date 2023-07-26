333bedf2e052d4cad775e2f5602b7c99338c05ca
BlackLotusJ <jerrykiche61@gmail.com>
2023-07-26 04:23:11 +0300

    Correcte parse.c

--- parse.c
+++ parse.c
@@ -14,37 +14,37 @@ int parse(const char *format, va_list args, int chars_printed)
 	{
 		case 'd':
 		case 'i':
-			chars_printed = printf_integer(args, chars_printed);
+			chars_printed = print_integer(args, chars_printed);
 			break;
 		case 'c':
 			_putchar(va_arg(args, int));
 			chars_printed++;
 			break;
 		case 's':
-			chars_printed = printf_string(args, chars_printed);
+			chars_printed = print_string(args, chars_printed);
 			break;
 		case '%':
 			_putchar('%');
 			chars_printed++;
 			break;
 		case 'b':
-			chars_printed = printf_binary(va_arg(args, unsigned int), chars_printed);
+			chars_printed = print_binary(va_arg(args, unsigned int), chars_printed);
 			break;
 		case 'x':
 		case 'X':
 			chars_printed = _x(va_arg(args, unsigned int), chars_printed, (*format == 'X') ? 1 : 0);
 			break;
 		case 'o':
-			chars_printed = printf_octal(va_arg(args, unsigned int), chars_printed);
+			chars_printed = print_octal(va_arg(args, unsigned int), chars_printed);
 			break;
 		case 'u':
-			chars_printed = printf_unsigned(va_arg(args, unsigned int), chars_printed);
+			chars_printed = print_unsigned(va_arg(args, unsigned int), chars_printed);
 			break;
 		case 'r':
-			chars_printed = printf_reverse(args, chars_printed);
+			chars_printed = print_reversed(args, chars_printed);
 			break;
 		case 'p':
-			chars_printed = printf_pointer(args, chars_printed);
+			chars_printed = print_pointer(args, chars_printed);
 			break;
 		default:
 			break;
